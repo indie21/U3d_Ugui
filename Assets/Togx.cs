70cca3306f749ede963b3340a19806492223b150
zhuoyikang <zhuoyikang@gmail.com>
2016-05-11 21:44:51 +0800

    08

---
+++ Assets/Togx.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Togx : MonoBehaviour {
+
+	public GameObject IsOn;
+	public GameObject IsOff;
+
+	private Toggle toggle;
+
+	// Use this for initialization
+	void Start () {
+		toggle = GetComponent<Toggle> ();
+		OnValueChange (toggle.isOn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnValueChange(bool x) {
+		IsOn.SetActive (x);
+		IsOff.SetActive (!x);
+	}
+}
